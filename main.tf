d17a4d7f5c97a5aa87618100cb38e800e4cbbf51
subash.t@finstein.ai <subash.t@finstein.ai>
2026-05-15 10:56:11 +0530

    Hardcode public SSH key for GitHub runner.

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "web_sg" {
 # NEW: Read the public key from your Ubuntu laptop and upload it to AWS
 resource "aws_key_pair" "ansible_key" {
   key_name   = "my_aws_key"
-  public_key = file("~/.ssh/my_aws_key.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyPimmDYJqSpRiY27Rhc01Xh0SGCyGsOFWAQNPJI8Z0sgDKdPnTYcwQYgup9KPm0gaXZ5nmayDQJYCLHjoYzdMwhy0o242wPWYvmxFlwPjO9kuQa3Y//E9ACLJvl1mWqML0dZB1lM3bIP2/daQDsNCbY87rWwIgtJHbhF9SKzPIL7sFO4WQDT8U1bwJs6tdo7rrf0Lq41GXgREPhj60ninLr+F8RVEneVXjX8w4JjsnGTkWsu+yOb6NiZZo73rRfK+8o7T/FSZI5Adb7iqCUcdFqWVewc/kR2BsmCTgUOdqnCY3SvsZwMEfDdVnpIe/1z7re6X4eFUv5WAbH1CrFurkhyo02SF4A04p3bE+qdoEz0fv2ulwlYUyz6ptApERrHCQ/t7kSHh7Pi4e1hlr0i0eZluF40TiPXuKxXZ1EUca3fmGMb3kcLjkzpfy2L/GEEBejQZHl0zr+g84kK3nRi0Bpi/o9qFHDdwydcaOuBfg5nHB6lLnCcfEMYc8XiB0yXBl8qwQmHnyFezd7j5NYZM0ljT4MX+lCHRC65MS4BJ9+JHh0zWswudbSdzWMP6pIkm/q1WxVvfe3xkK75TdScOgC3TaDJcp7jDP9dke16pUzHuB6BH5gv1JoxXD8chiqCTeC3YLvYLGz3klklYvjzPPi/trczaHzjYaAEjtnrgaw== finstein-emp@fin-pf46xkg6"
 }
 
 resource "aws_instance" "my_first_server" {
@@ -52,6 +52,7 @@ resource "aws_instance" "my_first_server" {
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
+  # NEW: Attach the padlock to the server
   key_name = aws_key_pair.ansible_key.key_name
 
   tags = {
